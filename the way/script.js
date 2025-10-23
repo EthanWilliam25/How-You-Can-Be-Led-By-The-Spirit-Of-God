4b2c886777ed20e29d552aa725de0fa441a865ca
Emperorwizdom <wizdomsmart25@gmail.com>
2025-10-23 10:11:52 +0530

    updated html and css files

---
+++ the way/script.js
@@ -0,0 +1,12 @@
+const currentMode = localStorage.getItem("mode");
+if (currentMode === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+function toggleMode() {
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem(
+    "mode",
+    document.body.classList.contains("dark-mode") ? "dark" : "light"
+  );
+}
